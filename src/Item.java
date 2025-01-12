07b4322fc76af76b9188b0f3e71c5af2592b64f3
Ali Tafakkori <alitafakkori82@gmail.com>
2025-01-12 17:11:16 +0330

    Create Item.java

---
+++ src/Item.java
@@ -0,0 +1,29 @@
+public class Item {
+    private final String id;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Item item = (Item) obj;
+        return id.equals(item.id);
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
